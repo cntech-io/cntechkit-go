bab5038e131225c4d7b8dc54adbaeef0a0bbf505
yunus <yunus@cntech.io>
2024-01-26 22:15:52 +0300

    new predefined envs

--- env/server_env.go
+++ env/server_env.go
@@ -7,17 +7,19 @@ import (
 )
 
 type ServerEnv struct {
-	DebugModeFlag  bool
-	AppPort        string
-	TrustedProxies []string
+	DebugModeFlag     bool
+	AppPort           string
+	TrustedProxies    []string
+	AccessTokenSecret string
 }
 
 type ServerEnvName string
 
 const (
-	DEBUG_MODE_FLAG ServerEnvName = "DEBUG_MODE_FLAG"
-	APP_PORT        ServerEnvName = "APP_PORT"
-	TRUSTED_PROXIES ServerEnvName = "TRUSTED_PROXIES"
+	DEBUG_MODE_FLAG     ServerEnvName = "DEBUG_MODE_FLAG"
+	APP_PORT            ServerEnvName = "APP_PORT"
+	TRUSTED_PROXIES     ServerEnvName = "TRUSTED_PROXIES"
+	ACCESS_TOKEN_SECRET ServerEnvName = "ACCESS_TOKEN_SECRET"
 )
 
 func NewServerEnv() *ServerEnv {
@@ -25,8 +27,9 @@ func NewServerEnv() *ServerEnv {
 		fmt.Println(".env file not found")
 	}
 	return &ServerEnv{
-		DebugModeFlag:  GetBoolean(string(DEBUG_MODE_FLAG), false),
-		AppPort:        GetString(string(APP_PORT), false),
-		TrustedProxies: GetStringArray(string(TRUSTED_PROXIES), ",", false),
+		DebugModeFlag:     GetBoolean(string(DEBUG_MODE_FLAG), false),
+		AppPort:           GetString(string(APP_PORT), false),
+		TrustedProxies:    GetStringArray(string(TRUSTED_PROXIES), ",", false),
+		AccessTokenSecret: GetString(string(ACCESS_TOKEN_SECRET), false),
 	}
 }
