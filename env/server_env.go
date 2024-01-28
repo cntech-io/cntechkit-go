e48bc1611e8978f712bb2a3c71f850fd89e213d8
yunus <yunus@cntech.io>
2024-01-28 23:35:49 +0300

    server env added

--- env/server_env.go
+++ env/server_env.go
@@ -7,19 +7,25 @@ import (
 )
 
 type ServerEnv struct {
-	DebugModeFlag     bool
-	AppPort           string
-	TrustedProxies    []string
-	AccessTokenSecret string
+	DebugModeFlag              bool
+	AppPort                    string
+	TrustedProxies             []string
+	AccessTokenSecret          string
+	RefreshTokenSecret         string
+	AccessTokenExpireDuration  int
+	RefreshTokenExpireDuration int
 }
 
 type ServerEnvName string
 
 const (
-	DEBUG_MODE_FLAG     ServerEnvName = "DEBUG_MODE_FLAG"
-	APP_PORT            ServerEnvName = "APP_PORT"
-	TRUSTED_PROXIES     ServerEnvName = "TRUSTED_PROXIES"
-	ACCESS_TOKEN_SECRET ServerEnvName = "ACCESS_TOKEN_SECRET"
+	DEBUG_MODE_FLAG               ServerEnvName = "DEBUG_MODE_FLAG"
+	APP_PORT                      ServerEnvName = "APP_PORT"
+	TRUSTED_PROXIES               ServerEnvName = "TRUSTED_PROXIES"
+	ACCESS_TOKEN_SECRET           ServerEnvName = "ACCESS_TOKEN_SECRET"
+	REFRESH_TOKEN_SECRET          ServerEnvName = "REFRESH_TOKEN_SECRET"
+	ACCESS_TOKEN_EXPIRE_DURATION  ServerEnvName = "ACCESS_TOKEN_EXPIRE_DURATION"
+	REFRESH_TOKEN_EXPIRE_DURATION ServerEnvName = "REFRESH_TOKEN_EXPIRE_DURATION"
 )
 
 func NewServerEnv() *ServerEnv {
@@ -27,9 +33,12 @@ func NewServerEnv() *ServerEnv {
 		fmt.Println(".env file not found")
 	}
 	return &ServerEnv{
-		DebugModeFlag:     GetBoolean(string(DEBUG_MODE_FLAG), false),
-		AppPort:           GetString(string(APP_PORT), false),
-		TrustedProxies:    GetStringArray(string(TRUSTED_PROXIES), ",", false),
-		AccessTokenSecret: GetString(string(ACCESS_TOKEN_SECRET), false),
+		DebugModeFlag:              GetBoolean(string(DEBUG_MODE_FLAG), false),
+		AppPort:                    GetString(string(APP_PORT), false),
+		TrustedProxies:             GetStringArray(string(TRUSTED_PROXIES), ",", false),
+		AccessTokenSecret:          GetString(string(ACCESS_TOKEN_SECRET), false),
+		RefreshTokenSecret:         GetString(string(REFRESH_TOKEN_SECRET), false),
+		AccessTokenExpireDuration:  GetNumber(string(ACCESS_TOKEN_EXPIRE_DURATION), false),
+		RefreshTokenExpireDuration: GetNumber(string(REFRESH_TOKEN_EXPIRE_DURATION), false),
 	}
 }
